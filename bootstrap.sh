5c697ce3f4eaca7ef0fc04a5d9915990f9d7f6c8
Aaron Graham <agraham19@gmail.com>
2014-05-31 11:52:44 +0100

    human readable rsync

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,21 +2,26 @@
 cd "$(dirname "${BASH_SOURCE}")"
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-		--exclude "README.md" --exclude "LICENSE-MIT.txt" -av --no-perms . ~
+		--exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~
 	source ~/.bash_profile
 }
 
 function dryRun() {
 	rsync --dry-run --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-		--exclude "README.md" --exclude "LICENSE-MIT.txt" -av --no-perms . ~
+		--exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~
 	source ~/.bash_profile
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt
 else
+	echo "Executing dry run..."
+	echo
+
 	dryRun
-	read -p "The files above will overwritten in your home directory. Are you sure you want to continue? (y/n) " -n 1
+	echo
+	echo
+	read -p "The files listed above will overwritten in your home directory. Are you sure you want to continue? (y/n) " -n 1
 	echo
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
 		doIt
