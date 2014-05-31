11d0eba918d06721ce13c8cd57305e1ddf228aaf
Aaron Graham <agraham19@gmail.com>
2014-05-31 11:37:18 +0100

    Improve the bootstrap script to display the files that are going to be overwritten first

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,10 +5,18 @@ function doIt() {
 		--exclude "README.md" --exclude "LICENSE-MIT.txt" -av --no-perms . ~
 	source ~/.bash_profile
 }
+
+function dryRun() {
+	rsync --dry-run --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
+		--exclude "README.md" --exclude "LICENSE-MIT.txt" -av --no-perms . ~
+	source ~/.bash_profile
+}
+
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+	dryRun
+	read -p "The files above will overwritten in your home directory. Are you sure you want to continue? (y/n) " -n 1
 	echo
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
 		doIt
